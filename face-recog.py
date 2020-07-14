9b0049b8e501f433e0f6d2a1d7433d003630c22c
Manan Bedi <53931841+manan-bedi2908@users.noreply.github.com>
2020-07-14 23:43:49 +0530

    Add files via upload

--- face-recog.py
+++ face-recog.py
@@ -1,20 +1,21 @@
+import urllib.request
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture("https://192.168.0.100:8080")
+url = "http://192.168.0.100:8080/shot.jpg"
 classifier = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 while True:
+    img_resp = urllib.request.urlopen(url)
+    img_arr = np.array(bytearray(img_resp.read()), dtype = np.uint8)
+    frame = cv2.imdecode(img_arr, -1)
+    faces = classifier.detectMultiScale(frame)
 
-    ret, frame = cap.read()
+    for face in faces:
+        x, y, w, h = face
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
 
-    if ret:
-        faces = classifier.detectMultiScale(frame)
-
-        for face in faces:
-            x, y, w, h = face
-            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
-
-        cv2.imshow("My Window", frame)
+    cv2.imshow("My Window", frame)
 
     key = cv2.waitKey(1000)
 
